0283143ee3aae1f59595337536d9665e5c2e52bb
dueon <hdejh79@gmail.com>
2025-10-03 18:49:53 -0700

    I'm done

--- dist/ex02.js
+++ dist/ex02.js
@@ -1,18 +1,17 @@
+"use strict";
 // 2. Create a function named `isPalindrome` that takes a string as a parameter.
 //    The function should return `true` if the string is a palindrome (reads the same backward as forward),
 //    otherwise, return `false`.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function isPalindrome(word: string) {
-  const reversed = word.split("").reverse().join("");
-
-  if (word === reversed) {
-    return true;
-  } else {
-    return false;
-  }
+function isPalindrome(word) {
+    const reversed = word.split("").reverse().join("");
+    if (word === reversed) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-
 // Expected output:
 console.log(isPalindrome("racecar")); // true
 console.log(isPalindrome("hello")); // false
--- dist/ex05.js
+++ dist/ex05.js
@@ -1,24 +1,21 @@
+"use strict";
 // 5. Create a function named `countVowels` that takes a string as a parameter.
 //    The function should return the number of vowels (a, e, i, o, u) in the string.
 //    Use TypeScript types to ensure the parameter and return type are properly typed.
-
-function countVowels(word: string) {
-  let count = 0;
-  let words = word.split("");
-  words.forEach((word) => {
-    if (
-      word === "a" ||
-      word === "e" ||
-      word === "i" ||
-      word === "o" ||
-      word === "u"
-    ) {
-      count++;
-    }
-  });
-  return count;
+function countVowels(word) {
+    let count = 0;
+    let words = word.split("");
+    words.forEach((word) => {
+        if (word === "a" ||
+            word === "e" ||
+            word === "i" ||
+            word === "o" ||
+            word === "u") {
+            count++;
+        }
+    });
+    return count;
 }
-
 // Expected output:
 console.log(countVowels("hello")); // 2
 console.log(countVowels("typescript")); // 2
